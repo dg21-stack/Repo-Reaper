4858e898f0fbfd6cf141e97612fb936178ded6dd
Max Pintchouk <maxpintchouk4321@gmail.com>
2025-02-27 11:30:45 -0500

    git diff route

--- git-scripts/main.py
+++ git-scripts/main.py
@@ -5,7 +5,8 @@ import os
 from methods.gitlog import run_git_log  
 from methods.getBranches import get_repo_branches
 from methods.gitReflog import get_reflog
-
+from methods.addCommitPush import add_commit_push
+from methods.gitDiff import git_diff
 app = Flask(__name__)
 
 @app.route('/')
@@ -44,6 +45,29 @@ def git_reflog():
     branch = data['branch']
     result = get_reflog(repo_path, branch)
     return jsonify({"repo-path": repo_path, "branch": branch, "reflog": result})
+
+@app.route('/add-commit-push', methods=['POST'])
+def git_add_commit_push():
+    data = request.get_json()
+    if not data or 'repo_path' not in data:
+        return jsonify({"error": "Missing repo_path in request body"}), 400
+    
+    repo_path = data['repo_path']
+    message = data['message']
+    branch = data['branch']
+    result = add_commit_push(repo_path, branch, message)
+    return jsonify({"repo-path": repo_path, "branch": branch, "message": message, "result": result})
+
+@app.route('/git-diff', methods=['POST'])
+def gitDiff():
+    data = request.get_json()
+    if not data or 'repo_path' not in data:
+        return jsonify({"error": "Missing repo_path in request body"}), 400
+    
+    repo_path = data['repo_path']
+    branch = data['branch']
+    result = git_diff(repo_path, branch)
+    return jsonify({"repo-path": repo_path, "branch": branch, "result": result})
 
 if __name__ == '__main__':
     app.run(debug=True)
